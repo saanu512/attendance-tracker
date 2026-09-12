76dc1fb2900cd5edc62459de0a74447bc3ca4eaf
saanu512 <154816187+saanu512@users.noreply.github.com>
2026-09-12 21:39:35 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "attendance-tracker-roshni-pearl-v44";
+const CACHE = "attendance-tracker-roshni-pearl-v46";
 const ASSETS = ["./","./index.html","./manifest.json","./styles.css","./app.js","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
