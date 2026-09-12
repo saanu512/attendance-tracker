0fc10c591ad8633aa3635f96c6569f66523cf45e
saanu512 <154816187+saanu512@users.noreply.github.com>
2026-09-12 18:24:46 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="attendance-tracker-v33-roshni";
+const CACHE="attendance-tracker-v35-roshni";
 const ASSETS=["./","./index.html","./manifest.json","./styles.css","./app.js","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
