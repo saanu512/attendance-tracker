de8b71f26727b530c9221c8d08fa0ef271a5f82a
saanu512 <154816187+saanu512@users.noreply.github.com>
2026-09-13 00:23:44 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "attendance-tracker-celestial-glass-v57";
+const CACHE = "attendance-tracker-full-app-v58";
 const ASSETS = ["./","./index.html","./manifest.json","./styles.css","./app.js","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
