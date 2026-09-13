fa4b8a3e632f812c5d39a8fbd04530cb5893895c
saanu512 <154816187+saanu512@users.noreply.github.com>
2026-09-13 22:26:46 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "attendance-tracker-full-app-v59";
+const CACHE = "attendance-tracker-full-app-v61";
 const ASSETS = ["./","./index.html","./manifest.json","./styles.css","./app.js","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
